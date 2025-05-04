e28670a44e44f08efdc9518ff808e2a5d0d14154
Cannoli558 <ronancannoli@gmail.com>
2025-05-04 15:09:43 -0400

    pizza

--- Assets/player.cs
+++ Assets/player.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
-
     private bool isMoving = false;
-    private Vector2 lastInput; // Stores last direction
+    private Vector2 lastInput = Vector2.zero; // Stores last direction
     private Animator animator;
 
     private void Awake()
@@ -14,41 +13,51 @@ public class PlayerController : MonoBehaviour
         animator = GetComponent<Animator>();
     }
 
+    private void Start()
+    {
+        // Set default animation state
+        animator.Play("idletree");
+    }
+
     private void Update()
     {
-        if (!isMoving)
+        // Get input regardless of movement state
+        Vector2 input = Vector2.zero;
+
+        // Manual check to prevent diagonal movement
+        if (Input.GetKey(KeyCode.W))
+            input.y = 1;
+        else if (Input.GetKey(KeyCode.S))
+            input.y = -1;
+        else if (Input.GetKey(KeyCode.A))
+            input.x = -1;
+        else if (Input.GetKey(KeyCode.D))
+            input.x = 1;
+
+        // Always update the animation parameters
+        if (input != Vector2.zero)
         {
-            Vector2 input = Vector2.zero;
+            // Save the last non-zero input
+            lastInput = input;
+        }
 
-            // Manual check to prevent diagonal movement
-            if (Input.GetKey(KeyCode.W))
-                input.y = 1;
-            else if (Input.GetKey(KeyCode.S))
-                input.y = -1;
-            else if (Input.GetKey(KeyCode.A))
-                input.x = -1;
-            else if (Input.GetKey(KeyCode.D))
-                input.x = 1;
+        // Update animator parameters - these are used by both blend trees
+        animator.SetFloat("moveX", lastInput.x);
+        animator.SetFloat("moveY", lastInput.y);
 
-            // If input was given
-            if (input != Vector2.zero)
-            {
-                lastInput = input;
-
-                // Match exactly your Animator parameters
-                animator.SetFloat("moveX", lastInput.x);
-                animator.SetFloat("moveY", lastInput.y);
-
-                // Move character
-                Vector3 targetPos = transform.position + new Vector3(lastInput.x, lastInput.y, 0f);
-                StartCoroutine(Move(targetPos));
-            }
+        // Switch between idle and walk trees based on movement state
+        if (input != Vector2.zero && !isMoving)
+        {
+            Vector3 targetPos = transform.position + new Vector3(input.x, input.y, 0f);
+            StartCoroutine(Move(targetPos));
         }
     }
 
     IEnumerator Move(Vector3 targetPos)
     {
+        // Start walking
         isMoving = true;
+        animator.Play("walktree");
 
         while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
         {
@@ -56,6 +65,7 @@ public class PlayerController : MonoBehaviour
             yield return null;
         }
 
+        // Return to idle
         transform.position = targetPos;
         isMoving = false;
     }
